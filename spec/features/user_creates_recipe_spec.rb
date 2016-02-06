0aebce15b3824caa27dfe212910c6fa1a7835e51
VictorMuramoto <victor.muramoto@gmail.com>
2016-02-06 03:34:34 -0200

    Created test - User create a recipe - success with image

--- spec/features/user_creates_recipe_spec.rb
+++ spec/features/user_creates_recipe_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 feature 'User creates a recipe' do
-  scenario 'success' do
+  scenario 'success withour image' do
     recipe = build(:recipe,
                    food_preference: create(:food_preference),
                    food_type: create(:food_type),
@@ -30,6 +30,38 @@ feature 'User creates a recipe' do
     expect(page).to have_content recipe.food_preference.name
     expect(page).to have_content recipe.kitchen.name
   end
+  scenario 'success with image' do
+    recipe = build(:recipe,
+                   food_preference: create(:food_preference),
+                   food_type: create(:food_type),
+                   kitchen: create(:kitchen))
+
+    visit new_recipe_path
+
+    fill_in 'recipe[name]', with: recipe.name
+    fill_in 'recipe[number_people]', with: recipe.number_people
+    fill_in 'recipe[time_prepare]', with: recipe.time_prepare
+    fill_in 'recipe[ingredients]', with: recipe.ingredients
+    fill_in 'recipe[description]', with: recipe.description
+    select  recipe.difficult, from: 'recipe[difficult]'
+    select  recipe.food_type.name, from: 'recipe[food_type_id]'
+    select  recipe.food_preference.name, from: 'recipe[food_preference_id]'
+    select  recipe.kitchen.name, from: 'recipe[kitchen_id]'
+    page.attach_file('recipe[image]', Rails.root + 'app/assets/images/strogonoff.jpg')
+
+    click_on 'submit'
+
+    expect(page).to have_content recipe.name
+    expect(page).to have_content recipe.number_people
+    expect(page).to have_content recipe.time_prepare
+    expect(page).to have_content recipe.ingredients
+    expect(page).to have_content recipe.description
+    expect(page).to have_content recipe.difficult
+    expect(page).to have_content recipe.food_type.name
+    expect(page).to have_content recipe.food_preference.name
+    expect(page).to have_content recipe.kitchen.name
+    expect(page).to have_css("img[src*='company_image.jpg']")
+  end
   scenario 'invalid data' do
     recipe = build(:recipe,
                    food_preference: create(:food_preference),
@@ -49,4 +81,5 @@ feature 'User creates a recipe' do
 
     expect(page).to have_content 'Cuidado! Alguns campos não podem ficar em branco'
   end
+
 end

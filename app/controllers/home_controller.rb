d003f119e4796c0a91b431ea813d8c116ba5bfe5
VictorMuramoto <victor.muramoto@gmail.com>
2016-02-06 16:09:57 -0200

    Implemented test - User see recipes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+end
--- spec/features/user/user_see_recipes_homepage_spec.rb
+++ spec/features/user/user_see_recipes_homepage_spec.rb
@@ -7,23 +7,39 @@ feature 'User sse recipes in homepage' do
     visit root_path
 
     expect(page).to have_content recipe1.name
-    expect(page).to have_content recipe1.number_people
-    expect(page).to have_content recipe1.time_prepare
-    expect(page).to have_content recipe1.ingredients
-    expect(page).to have_content recipe1.description
     expect(page).to have_content recipe1.difficult
     expect(page).to have_content recipe1.food_type.name
     expect(page).to have_content recipe1.food_preference.name
     expect(page).to have_content recipe1.kitchen.name
 
     expect(page).to have_content recipe2.name
-    expect(page).to have_content recipe2.number_people
-    expect(page).to have_content recipe2.time_prepare
-    expect(page).to have_content recipe2.ingredients
-    expect(page).to have_content recipe2.description
     expect(page).to have_content recipe2.difficult
     expect(page).to have_content recipe2.food_type.name
     expect(page).to have_content recipe2.food_preference.name
     expect(page).to have_content recipe2.kitchen.name
   end
+
+  scenario 'success with image' do
+    recipe1 = create(:recipe, image: File.new(
+                                              Rails.root + 'app/assets/images/strogonoff.jpg'))
+    recipe2 = create(:recipe,
+                     image: File.new(
+                                     Rails.root + 'app/assets/images/strogonoff.jpg'))
+    visit root_path
+
+    expect(page).to have_content recipe1.name
+    expect(page).to have_content recipe1.difficult
+    expect(page).to have_content recipe1.food_type.name
+    expect(page).to have_content recipe1.food_preference.name
+    expect(page).to have_content recipe1.kitchen.name
+    expect(page).to have_css("img[src*='image.jpg']")
+
+    expect(page).to have_content recipe2.name
+    expect(page).to have_content recipe2.difficult
+    expect(page).to have_content recipe2.food_type.name
+    expect(page).to have_content recipe2.food_preference.name
+    expect(page).to have_content recipe2.kitchen.name
+    expect(page).to have_css("img[src*='image.jpg']")
+  end
+
 end

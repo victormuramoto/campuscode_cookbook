31795a8a1c39a41d8a5f60862f4fcdac4ba5b767
VictorMuramoto <victor.muramoto@gmail.com>
2016-02-06 19:05:01 -0200

    admin creates kitchen - New Scenarios

--- spec/features/admin/admin_creates_kitchen_spec.rb
+++ spec/features/admin/admin_creates_kitchen_spec.rb
@@ -12,4 +12,22 @@ feature 'admin creates a kitchen' do
 
     expect(page).to have_content kitchen.name
   end
+  scenario 'Redirected to sign in page' do
+
+    visit new_kitchen_path
+
+    expect(page).to have_content 'Log in'
+    expect(page).to have_content 'Email'
+    expect(page).to have_content 'Password'
+  end
+  scenario "fields can't be blank" do
+    admin = login_admin
+    kitchen = build(:kitchen)
+
+    visit new_kitchen_path
+    click_on 'submit'
+
+    expect(page).to have_content t('flash.kitchens.create.alert')
+
+  end
 end

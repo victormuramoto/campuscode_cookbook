ff512942c693dcb7dcf959db82ede54db20d1cd6
VictorMuramoto <victor.muramoto@gmail.com>
2016-02-09 02:07:59 -0200

    Created test- User see most liked recipes at homepage

---
+++ spec/features/user/user_see_most_liked_recipes_homepage_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+feature 'User see most liked recipes in homepage' do
+  scenario 'success without login' do
+    user = create(:user)
+
+    recipe1 = create(:recipe,
+                      name:'Strogonoff frango',
+                      user:user,
+                      likes:10)
+    recipe2 = create(:recipe,
+                      name:'Bile a role',
+                      user:user,
+                      likes:8)
+    recipe3 = create(:recipe,
+                      name:'Pernil Assado',
+                      user:user,
+                      likes:5)
+    recipe4 = create(:recipe,
+                      name:'Peixe Espada',
+                      user:user,
+                      likes:7)
+    recipe5 = create(:recipe,
+                      name:'Folhado de Frango',
+                      user:user,
+                      likes:9)
+    recipe6 = create(:recipe,
+                      name:'Salmão com feijão',
+                      user:user,
+                      likes:2)
+    recipe7 = create(:recipe,
+                      name:'Arroz com brocolis',
+                      user:user,
+                      likes:4)
+    recipe8 = create(:recipe,
+                      name:'Pão de queijo no alho',
+                      user:user,
+                      likes:1)
+
+    visit root_path
+
+    expect(page).to have_content recipe1.name
+    expect(page).to have_content recipe2.name
+    expect(page).to have_content recipe3.name
+    expect(page).to have_content recipe4.name
+    expect(page).to have_content recipe5.name
+    expect(page).to_not have_content recipe6.name
+    expect(page).to_not have_content recipe7.name
+    expect(page).to_not have_content recipe8.name
+  end
+end

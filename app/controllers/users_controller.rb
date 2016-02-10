db9607b3a3923bad88e59dd3b3891593bc5721bf
VictorMuramoto <victor.muramoto@gmail.com>
2016-02-10 17:43:45 -0200

    Implemented tests - User See other profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  before_action :set_kitchens, only: [:show]
+
 
   def show
-    
+  end
+
+
+  private
+
+  def set_kitchens
+    @user_kitchens =  KitchensUser.all.where("user_id = #{params[:id]}")
+    @kitchens = []
+    if @user_kitchens != []
+      @user_kitchens.each do |k|
+        @kitchens  << Kitchen.find(k.kitchen_id)
+      end
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- spec/features/user/user_see_other_users_spec.rb
+++ spec/features/user/user_see_other_users_spec.rb
@@ -34,4 +34,66 @@ feature 'User see other users profile' do
     expect(page).to have_content recipe.user.facebook
     expect(page).to have_content recipe.user.twitter
   end
+  scenario 'show kitchens' do
+    user = login_user
+    kitchen1 = create(:kitchen, name:'Paulista')
+    kitchen2 = create(:kitchen, name:'Gaucha')
+    kitchen3 = create(:kitchen, name:'Nordestina')
+    recipe = create(:recipe, user: create(:user,
+                                          first_name:'Joao',
+                                          last_name: 'Das Neves',
+                                          password:'eunaoseinada',
+                                          password_confirmation:'eunaoseinada',
+                                          email:'nada@eusei.com.br'))
+
+    create(:kitchens_user, kitchen_id: kitchen1.id,
+                           user_id: recipe.user.id)
+    create(:kitchens_user, kitchen_id: kitchen2.id,
+                           user_id: recipe.user.id)
+    create(:kitchens_user, kitchen_id: kitchen3.id,
+                           user_id: recipe.user.id)
+
+    visit recipe_path(recipe)
+
+    click_on recipe.user.first_name
+
+    expect(page).to have_content recipe.user.first_name
+    expect(page).to have_content recipe.user.last_name
+    expect(page).to have_content recipe.user.email
+    expect(page).to have_content recipe.user.city
+    expect(page).to have_content recipe.user.facebook
+    expect(page).to have_content recipe.user.twitter
+    expect(page).to have_content kitchen1.name
+    expect(page).to have_content kitchen2.name
+    expect(page).to have_content kitchen3.name
+
+  end
+  scenario 'show all recipes' do
+    user = login_user
+    recipe = create(:recipe, user: create(:user,
+                                          first_name:'Joao',
+                                          last_name: 'Das Neves',
+                                          password:'eunaoseinada',
+                                          password_confirmation:'eunaoseinada',
+                                          email:'nada@eusei.com.br'))
+
+    visit recipe_path(recipe)
+
+    click_on recipe.user.first_name
+
+    expect(page).to have_content recipe.user.first_name
+    expect(page).to have_content recipe.user.last_name
+    expect(page).to have_content recipe.user.email
+    expect(page).to have_content recipe.user.city
+    expect(page).to have_content recipe.user.facebook
+    expect(page).to have_content recipe.user.twitter
+
+    user_owner = recipe.user
+
+    user_owner.recipes.each do |recipe|
+      expect(page).to have_content recipe.name
+      expect(page).to have_content recipe.ingredients
+      expect(page).to have_content recipe.description
+    end
+  end
 end
